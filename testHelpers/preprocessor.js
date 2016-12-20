28969b11920800dd2037c2285a52b4dd6ec7845d
Bryan Nicholls <book.man25@gmail.com>
2016-12-19 19:49:36 -0500

    updated version of jest

--- testHelpers/preprocessor.js
+++ testHelpers/preprocessor.js
@@ -5,23 +5,20 @@ const tsc = require("typescript");
 
 module.exports = {
     process(src, path) {
-        if (path.endsWith('.ts') || path.endsWith('.tsx')) {
-            src = src.replace(/((import.*)|(.*require\()|(unmock\()|(mock\())["'](.*)["']/g,
-                (fullPattern, leftSide, importSide, requireSide, unmockSide, mockSide, rightSide) => leftSide + "\"" + updatePath(path, rightSide) + "\"");
-            const transpiled = tsc.transpileModule(
-              src,
-              {
-				  compilerOptions: {
-					  module: tsc.ModuleKind.CommonJS,
-					  jsx: tsc.JsxEmit.React,
-					  noEmitHelpers: true
-				  },
-				  fileName: path
-              }
-            );
-			return `require('ts-jest').install({environment: 'node', emptyCacheBetweenOperations: true});${transpiled.outputText}`;
-        }
-        return src;
+        src = src.replace(/((import.*)|(.*require\()|(unmock\()|(mock\())["'](.*)["']/g,
+            (fullPattern, leftSide, importSide, requireSide, unmockSide, mockSide, rightSide) => leftSide + "\"" + updatePath(path, rightSide) + "\"");
+        const transpiled = tsc.transpileModule(
+            src,
+            {
+                compilerOptions: {
+                    module: tsc.ModuleKind.CommonJS,
+                    jsx: tsc.JsxEmit.React,
+                    noEmitHelpers: true
+                },
+                fileName: path
+            }
+        );
+        return `require('ts-jest').install({environment: 'node', emptyCacheBetweenOperations: true});${transpiled.outputText}`;
     },
     getCacheKey: createCacheKeyFunction([__filename])
 };
